2f2bc0c217a091dc5e5f6637e910dd367d547fc2
nagaraju12333 <136324496+nagaraju12333@users.noreply.github.com>
2023-11-03 08:51:16 +0000

    Add files via upload

---
+++ javas.js
@@ -0,0 +1,32 @@
+function v()
+{
+    var m=document.getElementById(1);
+    var n=document.getElementById(2);
+
+    if(m.value=="" ||n.value=="");
+    {
+        alert("your form is empty");
+        return true;
+    }
+    if (n.value<10) 
+    {
+        alert("passwd is lessor than ")
+        return false;
+
+
+    }
+    else
+    return true;
+        
+    }
+
+    
+    
+
+
+    
+
+    
+
+
+
